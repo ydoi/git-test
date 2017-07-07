227b21532abfb23ba644b08aee83b0b44468ad06
ydoi <yu-doi@rhizome-dev.sakura.ne.jp>
2017-07-07 10:33:44 +0900

    modify 読み込み処理を追加

--- src/main/java/own.java
+++ src/main/java/own.java
@@ -1,1 +1,10 @@
-/* test */
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        for(;cin.hasNext();) {
+            System.out.println(cin.nextLine());
+        }
+    }
+}
